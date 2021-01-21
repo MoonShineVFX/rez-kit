6ac77583248e7c7313c041888b69b47915f3c5a3
David Lai <davidlatwe@gmail.com>
2021-01-21 23:29:02 +0800

    install rez with required pip packages

--- install.py
+++ install.py
@@ -1,24 +1,45 @@
 
 import os
 import sys
+import venv
 import functools
 import subprocess
 from kitz import git, lib
 
 
 REZ_URL = "https://github.com/nerdvegas/rez.git"
+REZ_SRC = "build/rezsrc"
 
 
 def install_rez(dst):
-    rezsrc = "build/rezsrc"
-    git.clone(REZ_URL, rezsrc)
+    git.clone(REZ_URL, REZ_SRC)
 
     if os.path.isdir(dst):
         lib.clean(dst)
     os.makedirs(dst)
 
     subprocess.check_call([sys.executable, "install.py", "-v", dst],
-                          cwd=rezsrc)
+                          cwd=REZ_SRC)
+
+    install_pip_packages(dst)
+
+
+def install_pip_packages(dst):
+    py_executable = os.path.join(get_virtualenv_bin_dir(dst),
+                                 os.path.basename(sys.executable))
+    subprocess.check_call([
+        py_executable, "-m", "pip", "install",
+        "pyside2",
+        "qt.py",
+        "qt5.py",
+        "pymongo",
+    ])
+
+
+def get_virtualenv_bin_dir(dest_dir):
+    builder = venv.EnvBuilder()
+    context = builder.ensure_directories(dest_dir)
+    return context.bin_path
 
 
 if __name__ == "__main__":
