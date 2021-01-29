ab75df85378b7dc4389614195926eee455d03a4b
David Lai <davidlatwe@gmail.com>
2021-01-29 21:31:17 +0800

    add profile prefix

--- allzparkconfig.py
+++ allzparkconfig.py
@@ -16,7 +16,7 @@ def profiles():
     from rez.packages import get_latest_package_from_string
     from rez.package_repository import package_repository_manager
 
-    _memory = "memory@any"
+    _memory = "memory@allzpark.profiles"
     _suites = "production-1"
     _profiles = []
 
@@ -26,19 +26,22 @@ def profiles():
     suites = get_latest_package_from_string(_suites)
 
     profile_pkgs = dict()
-    for name in os.listdir(suites.suite_root):
-        maker = PackageMaker(name, data=suites.data.copy())
+    for show_name in os.listdir(suites.suite_root):
+        # add prefix so the profile name won't conflict with regular packages
+        pkg_name = suites.mem_pkg_prefix + show_name
+
+        maker = PackageMaker(pkg_name, data=suites.data.copy())
         package = maker.get_package()
         data = package.data.copy()
 
         requires = data.get("requires", [])
-        requires.append(".show-%s" % name)
+        requires.append(".show-%s" % show_name)
 
         data["requires"] = requires
         data["version"] = "1"
-        profile_pkgs[name] = {"1": data}
+        profile_pkgs[pkg_name] = {"1": data}
 
-        _profiles.append(name)
+        _profiles.append(pkg_name)
 
     memory_repo = package_repository_manager.get_repository(_memory)
     memory_repo.data = profile_pkgs
