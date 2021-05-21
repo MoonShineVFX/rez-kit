5774e4015a390600aef1024541b7392b2d6da290
David Lai <davidlatwe@gmail.com>
2021-05-21 23:47:16 +0800

    fix module not found in build

--- bootstrap/rezutil/package.py
+++ bootstrap/rezutil/package.py
@@ -1,11 +1,16 @@
 
 name = "rezutil"
 
-version = "1.4.6-m1"
+version = "1.4.6-m2"
 
 build_command = "python {root}/rezbuild.py {install}"
 
 
 def commands():
     env = globals()["env"]
+    building = globals()["building"]
+
     env.PYTHONPATH.prepend("{root}/python")
+    if building:
+        # PYTHONPATH will be ignored in production installed rez
+        env.REZ_PACKAGE_DEFINITION_BUILD_PYTHON_PATHS.prepend("{root}/python")
