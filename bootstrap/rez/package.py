7e9fdf393a63e879a4fed49037cd93aea1d12994
davidlatwe.lai <davidlatwe.lai@gmail.com>
2021-06-15 20:27:14 +0800

    update rez

--- bootstrap/rez/package.py
+++ bootstrap/rez/package.py
@@ -4,23 +4,37 @@ name = "rez"
 
 description = "Rez itself as package"
 
+git_url = "https://github.com/MoonShineVFX/rez.git"
+
 
 @early()
 def version():
     import os
-    import rez
 
-    os.environ["REZ_SOURCE_PATH"] = rez.__path__[0]
+    package_ver = "m1"
+    payload_ver = os.getenv("REZ_DELIVER_PKG_PAYLOAD_VER")
 
-    # Our fork of rez has '+' sign in version string to indicate
-    # local change.
-    # Which align to PEP 440 version spec but not to rez package's
-    # version syntax. So we have to normalize it.
-    return rez.__version__.replace("+", "-")
+    if payload_ver:
+        # Our fork of rez has '+' sign in version string to indicate
+        # local change.
+        # Which align to PEP 440 version spec but not to rez package's
+        # version syntax. So we have to normalize it.
+        payload_ver.replace("+", "-")
+
+        return "%s-%s" % (
+            # payload version
+            payload_ver,
+            # package def version
+            package_ver
+        )
+
+    else:
+        return "0.0.0-" + package_ver
 
 
 variants = [
-    ["platform-*", "arch-*", "os-*", "python-*.*"],
+    ["platform-*", "arch-*", "os-*", "python-2.7"],
+    ["platform-*", "arch-*", "os-*", "python-3.7"],
 ]
 
 # NOTE: This build script is simply copying modules from rez source,
