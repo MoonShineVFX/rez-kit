89eaffdae8fac0d6aa3b1e9f0608445d209e0396
David Lai <davidlatwe@gmail.com>
2021-07-08 18:53:26 +0800

    [arnold] add arnold core version env

--- production/arnold_core/5.1.1.0/package.py
+++ production/arnold_core/5.1.1.0/package.py
@@ -4,3 +4,10 @@ name = "arnold_core"
 version = "5.1.1.0-m1"
 
 build_command = False
+
+
+def pre_commands():
+    env = globals()["env"]
+
+    env.ARNOLD_CORE_VERSION = \
+        str(env.REZ_ARNOLD_CORE_VERSION).rsplit("-m", 1)[0]
--- production/arnold_core/6.0.3.0/package.py
+++ production/arnold_core/6.0.3.0/package.py
@@ -4,3 +4,10 @@ name = "arnold_core"
 version = "6.0.3.0-m1"
 
 build_command = False
+
+
+def pre_commands():
+    env = globals()["env"]
+
+    env.ARNOLD_CORE_VERSION = \
+        str(env.REZ_ARNOLD_CORE_VERSION).rsplit("-m", 1)[0]
