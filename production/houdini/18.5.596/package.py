2194c2619fce6b4e46044800bc758bf10ae2c5bd
David Lai <davidlatwe@gmail.com>
2021-07-12 14:50:11 +0800

    [houdini] add versions

---
+++ production/houdini/18.5.596/package.py
@@ -0,0 +1,26 @@
+
+name = "houdini"
+
+version = "18.5.596-m4"
+
+description = "SideFX Houdini"
+
+_data = {
+    "label": "Houdini 18.5.596",
+    "icon": "{root}/resources/icon.svg",
+    "level": "task",
+    "role": None,
+}
+
+requires = [
+    "!PySide2",
+    "houbase-1",
+]
+
+tools = [
+    "houdinifx",
+]
+
+
+private_build_requires = ["rezutil-1"]
+build_command = "python -m rezutil build {root}"
