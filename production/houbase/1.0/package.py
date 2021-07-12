ecf8364348fedba9afb81c3217f2bca1239c1f68
David Lai <davidlatwe@gmail.com>
2021-07-12 14:00:57 +0800

    [houdini] update base env

--- production/houbase/1.0/package.py
+++ production/houbase/1.0/package.py
@@ -45,13 +45,40 @@ def commands():
     # Disable local .env file
     env.HOUDINI_NO_ENV_FILE = "1"
 
+    # Expands to sub-dir of HOUDINI_PATH
+    env.HOUDINI_MENU_PATH.append("@")
+    env.HOUDINI_VEX_PATH.append("@/vex")
+    env.HOUDINI_OCL_PATH.append("@/ocl")
+    env.HOUDINI_GLSL_PATH.append("@/ogl2")
+    env.HOUDINI_OTLSCAN_PATH.append("@/otls")
+
     # Append "default" path for the given variable
     env.HOUDINI_PATH.append("&")
+    env.HOUDINI_DSO_PATH.append("&")
+    env.HOUDINI_SOHO_PATH.append("&")
     env.HOUDINI_SCRIPT_PATH.append("&")
+    env.HOUDINI_OTLSCAN_PATH.append("&")
+    env.HOUDINI_TOOLBAR_PATH.append("&")
+    env.HOUDINI_UI_PATH.append("&")
+    env.HOUDINI_UI_ICON_PATH.append("&")
 
-    # Expands to sub-dir of HOUDINI_PATH
-    env.HOUDINI_MENU_PATH.append("@/")
-    env.HOUDINI_OTLSCAN_PATH.append("@/otls")
+    # Force using houdini built-in python (deprecated)
+    # env.HOUDINI_USE_HFS_PYTHON = "1"
+
+    # Dev use, enables more verbose console output
+    # env.HOUDINI_SCRIPT_DEBUG = "1"
+    # env.HOUDINI_SOHO_DEVELOPER = "1"
+
+    # Use default browser on the system to open the help URLs (windows)
+    # env.HOUDINI_EXTERNAL_HELP_BROWSER = "start"
+
+    # Determines if DSO/DLL errors on plug-ins will be printed out to
+    # the console.
+    # "1" : dynamic linking errors to be output
+    # "2" : more verbose errors to be printed
+    # "3" : all DSO related messages to be printed
+    # "4" : timing messages to be printed
+    # env.HOUDINI_DSO_ERROR = "2"
 
     # version range specific
     if hou_version_info[:2] >= [17, 5]:
