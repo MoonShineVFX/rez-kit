0aef983b19e901c9a0169f22cf49d399d30681f5
David Lai <davidlatwe@gmail.com>
2021-07-13 21:38:09 +0800

    [sidefxlabs] improve package build

--- production/hou_sidefxlabs/package.py
+++ production/hou_sidefxlabs/package.py
@@ -1,13 +1,46 @@
 
 name = "hou_sidefxlabs"
 
-version = "18.5.447-m1"
+description = \
+    """
+    SideFX Labs is a completely free, open-source toolset geared towards 
+    assisting Houdini users with a variety of tasks commonly used for 
+    digital content creation. It is an all-inclusive toolset that spans 
+    the shelf, digital assets, custom desktops and scripts and more. The 
+    toolset is currently maintained by Paul Ambrosiussen and Mai Ao. It 
+    also receives a lot of contributions from the always-active Houdini 
+    community. The toolset originates from the GameDevelopmentToolset, 
+    which got a re-launch in the Houdini 18.0 release.
+    """
 
-description = "See https://github.com/sideeffects/SideFXLabs "\
-              "and https://www.sidefx.com/products/sidefx-labs/"
+git_url = "https://github.com/sideeffects/SideFXLabs.git"
+
+
+@early()
+def version():
+    import os
+
+    package_ver = "m2"
+    payload_ver = os.getenv("REZ_DELIVER_PKG_PAYLOAD_VER")
+
+    if payload_ver:
+        return "%s-%s" % (
+            # payload version
+            payload_ver,
+            # package def version
+            package_ver
+        )
+
+    else:
+        return "0.0.0-" + package_ver
+
 
 requires = [
-    "houdini-18.0+<18.6",
+    # `SideFXLabs.json` has a field 'enable' that also checks houdini
+    # version which will disable the package if not compatible.
+    # So we just leave this rez requirement as minimum as what SideFXLabs
+    # says.
+    "houdini-18.0+",
 ]
 
 private_build_requires = ["rezutil-1"]
@@ -16,7 +49,14 @@ build_command = "python {root}/rezbuild.py {install}"
 
 def commands():
     env = globals()["env"]
+    this = globals()["this"]
 
-    env.SIDEFXLABS = "{root}/payload"
-    env.PATH.prepend("{root}/payload/bin")
-    env.HOUDINI_PATH.prepend("{root}/payload")
+    payload_ver = "{}.{}.{}".format(*this.version.as_tuple()[:3])
+
+    env.HOUDINI_PACKAGE_DIR.prepend("{root}/payload")
+
+    env.SIDEFXLABS_ADMIN_UPDATES = "1"
+    env.SIDEFXLABS_NOINSTALL_MESSAGE = (
+        "SideFX Labs (%s) is provide by Rez and cannot be changed "
+        "in current session." % payload_ver
+    )
