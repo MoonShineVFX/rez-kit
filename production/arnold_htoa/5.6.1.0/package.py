adb7467b10f8d9d75c2cc639218acfd3668de8fd
David Lai <davidlatwe@gmail.com>
2021-07-14 12:35:13 +0800

    [arnold] add htoa-5.6.1.0

---
+++ production/arnold_htoa/5.6.1.0/package.py
@@ -0,0 +1,38 @@
+
+name = "arnold_htoa"
+
+version = "5.6.1.0-m1"
+
+_data = {
+    # Allzpark
+    "label": "HtoA",
+    "icon": "{root}/htoa.ico"
+}
+
+requires = [
+    "arnold_core-6.2.1.0",
+    "~usd_arnold-6.2.1.0",
+    "~openvdb-4.0.0",
+]
+
+variants = [
+    ["platform-*", "houdini-17.5.460"],
+    ["platform-*", "houdini-18.0.597"],
+    ["platform-*", "houdini-18.5.532"],
+    ["platform-*", "houdini-18.5.563"],
+    ["platform-*", "houdini-18.5.596"],
+]
+
+tools = [
+    "hick",
+    "kick",
+    "oslc",
+    "oslinfo",
+    "oiiotool",
+    "maketx",
+    "noice",
+]
+
+
+private_build_requires = ["rezutil-1"]
+build_command = "python -m rezutil build {root} --use-zip"
